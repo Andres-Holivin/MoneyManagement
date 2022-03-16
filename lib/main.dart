cd7c1682fbe8f2fe950b76286c5bb6008331533f
andres holivin <andres12holivin@gmail.com>
2022-03-16 14:40:11 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:money_management/util/app_router.gr.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+final _appRouter = AppRouter();
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerDelegate: _appRouter.delegate(),
+      routeInformationParser: _appRouter.defaultRouteParser(),
+    );
+  }
+}
---
+++ lib/util/constants.dart
@@ -0,0 +1,6 @@
+class ConstBackground {
+  static const String PRIMARY = '#222831';
+  static const String SECONDARY = '#393E46';
+  static const String TERTIARY = '#00ADB5';
+  static const String QUATERNARY = '#EEEEEE';
+}
